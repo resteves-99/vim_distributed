a666f6c34703a4a975070a0a3c43853b2857d8d3
resteves-99 <rafaele675@gmail.com>
2022-05-16 18:14:06 -0700

    changed file

--- src/bufwrite.c
+++ src/bufwrite.c
@@ -641,11 +641,9 @@ buf_write(
 	// maybe TODO
 		// I can use buf and fopen/fwrite to create the file (with fname) here.
 	// char_u last_name = 0;
-	FILE* debug_file = fopen("~/debug/test1.txt", "w");
-	if (debug_file != NULL) {
-		fprintf(debug_file, "start of buf_write");
-		fclose(debug_file);
-	}
+	FILE* debug_file_tmp = fopen("./debug/test3.txt", "w");
+	fprintf(debug_file_tmp, "start of buf_write");
+	fclose(debug_file_tmp);
 	
 
     int		    fd;
